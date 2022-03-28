c02de39dc7e53ecbca71fb5fbdb54c6b1350f008
Ruben Muanza <galliano1986@live.nl>
2022-03-28 19:03:41 +0200

    project ruben

---
+++ src/main/java/com/ruben/springprojectguru/SpringprojectGuruApplication.java
@@ -0,0 +1,13 @@
+package com.ruben.springprojectguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringprojectGuruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringprojectGuruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ruben/springprojectguru/SpringprojectGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruben.springprojectguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringprojectGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
